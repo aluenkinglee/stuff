87fd76fe08fface0882e66d6aa8edf97e70e0623
aluenkinglee <aluenkinglee@gmail.com>
2014-03-10 20:05:39 +0800

    删除了多余的调试代码

--- cplusplus/feature_extractor/main.cpp
+++ cplusplus/feature_extractor/main.cpp
@@ -181,54 +181,37 @@ Flows flowpool;
 void loop_callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
     static int count = 0;                           //包计数器
-    const struct packet_ethernet *ethernet;     //The ethernet header [1]
     const struct packet_ip   *ip;                 //the IP header
-    const struct packet_tcp *tcp;                   //The TCP header */
     const struct udp_header *udp;
-    //u_char *payload;                                //Packet payload */
 
     int size_ip;
     int size_tcp;
-    int size_udp;
-    int size_payload;
+
 
     count++;
 
     Packet p(header,packet);
-
-    /* 以太网头 */
-    ethernet = (struct packet_ethernet*)(packet);
-
     /* IP头 */
-
     Packet_ip iptest =p.IP();
     size_ip = iptest.size();
 
-    /*
-    cout << "size_ip:"<<iptest.size() <<endl;
-    cout << "ip_protocal:" << iptest.get_protocal() << endl;
-    if ( size_ip < 20)
-    {
-        printf("无效的IP头长度: %u bytes\n", size_ip);
-        return;
-    }
-    */
     ip = (struct packet_ip*)(packet + SIZE_ETHERNET);
-    //size_ip = IP_HL(ip)*4;
+
     if (size_ip < 20)
     {
         printf("无效的IP头长度: %u bytes\n", size_ip);
         return;
     }
 
-    switch (ip->ip_p)
+    switch (iptest.get_protocal())
     {
     case IPPROTO_TCP:
     {
         /* TCP头 */
-        tcp = (struct packet_tcp*)(packet + SIZE_ETHERNET + size_ip);
-        size_tcp = TH_OFF(tcp)*4;
+        //tcp = (struct packet_tcp*)(packet + SIZE_ETHERNET + size_ip);
+
         Packet_tcp tcptest = p.TCP();
+        size_tcp = tcptest.size();
         if (size_tcp < 20)
         {
             //printf("无效的TCP头长度: %u bytes\n", size_tcp);
@@ -281,43 +264,12 @@ void loop_callback(u_char *args, const struct pcap_pkthdr *header, const u_char 
         cout << "len:"<<p.get_header().len
             <<" payload:"<< tcptest.payload_length()<<"bytes"<<endl;
 */
-
-
-/*
-        int sport =  ntohs(tcp->th_sport);
-        int dport =  ntohs(tcp->th_dport);
-
-        printf("%s:%d -> ", inet_ntoa(ip->ip_src), sport);
-        printf("%s:%d ", inet_ntoa(ip->ip_dst), dport);
-
-
-        printf("[TCP] ");
-        if ( Dissector::is_tcp_sec(tcp->th_flags))
-        {
-            printf("[SYN ACK] ");
-        }
-        else if (Dissector::is_tcp_begin(tcp->th_flags))
-        {
-            printf("[SYN] ");
-        }
-        else if ( Dissector::is_tcp_third(tcp->th_flags))
-        {
-            printf("[ACK] ");
-        }
-
-        //内容
-        //payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-        //内容长度
-        size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-        printf("len=%u seq=%u ack=%u payload=%dbytes \n", header->len,ntohs(tcp->th_seq), ntohs(tcp->th_ack), size_payload );
-*/
         break;
     }
 
     case IPPROTO_UDP:
     {
         udp = (udp_header*) (packet + SIZE_ETHERNET + size_ip);
-        size_udp = 8;// see http://6152587.blog.51cto.com/6142587/1229425
 
         int sport =  ntohs(udp->uh_sport);
         int dport =  ntohs(udp->uh_dport);
@@ -352,7 +304,6 @@ double time_diff(struct timeval x , struct timeval y);
 void feature_extractor(flow f)
 {
     struct pcap_pkthdr header; // The header that pcap gives us
-    const u_char *packet; // The actual packet
 
     struct timeval cur_ts = {0,0};  //current timestamp
     struct timeval pre_ts =  {0,0};  //previous timestamp
@@ -393,16 +344,14 @@ void feature_extractor(flow f)
         string dip = ip.dest_ip();
         if(ip.get_protocal() == IPPROTO_TCP) {
             Packet_tcp tcp = p.TCP();
-            sprintf(buffer,"%s %s %s %d %d %d %d",
+            sprintf(buffer,"%d %s %s %hd %hd %lu",
                     ip.get_protocal(),
                     sip.c_str(),
                     dip.c_str(),
                     tcp.sport(),
                     tcp.dport(),
-                    tcp.payload_length(),
-                    tcp.window());
+                    tcp.payload_length());
             features.push_back(buffer);
-
         } else if (ip.get_protocal() == IPPROTO_UDP) {
         //udp
             features.push_back(buffer);
@@ -412,117 +361,8 @@ void feature_extractor(flow f)
 
 }
 
-int standord_test(int argc, char **argv)
-{
-    vector<Packet> temp;
-    unsigned int pkt_counter=0;   // packet counter
-    unsigned long byte_counter=0; //total bytes seen in entire trace
-    unsigned long cur_counter=0; //counter for current 1-second interval
-    unsigned long max_volume = 0;  //max value of bytes in one-second interval
-
-
-    struct timeval cur_ts = {0,0};  //current timestamp
-    struct timeval pre_ts =  {0,0};  //previous timestamp
-
-
-
-    struct pcap_pkthdr header; // The header that pcap gives us
-    const u_char *packet; // The actual packet
-
-    //check command line arguments
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s [input pcaps]\n", argv[0]);
-        exit(1);
-
-    }
-
-    //-------- Begin Main Packet Processing Loop -------------------
-    //loop through each pcap file in command line args
-    for (int fnum=1; fnum < argc; fnum++)
-    {
-
-        //-----------------
-        //open the pcap file
-        pcap_t *handle;
-        char errbuf[PCAP_ERRBUF_SIZE]; //not sure what to do with this, oh well
-        handle = pcap_open_offline(argv[fnum], errbuf);   //call pcap library function
-
-        if (handle == NULL)
-        {
-            fprintf(stderr,"Couldn't open pcap file %s: %s\n", argv[fnum], errbuf);
-            return(2);
-        }
-
-        //-----------------
-        //begin processing the packets in this particular file, one at a time
-
-        while (packet = pcap_next(handle,&header))
-        {
-            // header contains information about the packet (e.g. timestamp)
-
-            u_char *pkt_ptr = (u_char *)packet; //cast a pointer to the packet data
-            //Packet p(&header,packet);
-            //cout << "vector size:"<<temp.size() << endl;
-            //temp.push_back(p);
-
-            //parse the first (ethernet) header, grabbing the type field
-            int ether_type = ((int)(pkt_ptr[12]) << 8) | (int)pkt_ptr[13];
-            int ether_offset = 0;
-
-            if (ether_type == ETHER_TYPE_IP) //most common
-                ether_offset = 14;
-            else if (ether_type == ETHER_TYPE_8021Q) //my traces have this
-                ether_offset = 18;
-            else
-                fprintf(stderr, "Unknown ethernet type, %04X, skipping...\n", ether_type);
-
-            //parse the IP header
-            pkt_ptr += ether_offset;  //skip past the Ethernet II header
-            struct ip *ip_hdr = (struct ip *)pkt_ptr; //point to an IP header structure
-
-            int packet_length = ntohs(ip_hdr->ip_len);
-
-            //check to see if the next second has started, for statistics purposes
-            if (cur_ts.tv_sec == 0)    //this takes care of the very first packet seen
-            {
-                printf("%.6lf\n", time_diff(pre_ts,cur_ts));
-                pre_ts = cur_ts;
-                cur_ts = header.ts;
-            }
-            else
-            {
-                pre_ts = cur_ts;
-                cur_ts = header.ts;   //update time interval
-                printf("%.6lf\n", time_diff(pre_ts,cur_ts));
-            }
-
-            cur_counter += packet_length;
-            byte_counter += packet_length; //byte counter update
-            pkt_counter++; //increment number of packets seen
-
-
-        } //end internal loop for reading packets (all in one file)
-
-        pcap_close(handle);  //close the pcap file
-
-
-    } //end for loop through each command line argument
-
-    //---------- Done with Main Packet Processing Loop --------------
-
-    //output some statistics about the whole trace
-    byte_counter /= 1e6;  //convert to MB to make easier to read
-
-    printf("Processed %d packets and %u MBytes, in %d files\n", pkt_counter, byte_counter, argc-1);
-    return 0;
-}
-//-------------------------------------------------------------------
 int run(int argc, char **argv)
 {
-    //struct pcap_pkthdr header; // The header that pcap gives us
-    //const u_char *packet; // The actual packet
-
     //open the pcap file
     pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE]; //not sure what to do with this, oh well
